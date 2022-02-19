f578cad0009f8da345e21bacd7a7923e9c6a6249
anjali <markandeanjali@gmail.com>
2022-02-19 14:02:40 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -10,51 +10,51 @@ import SearchBox from './SearchBox/SearchBox';
 function App() {
 
   const [names, setNames] = useState([]);
-  const[searchField,setSearchField]=useState('');
-  const[isOpenPopup,setOpenPopup]=useState(false);
-  const[clickedDog,setClickedDog]=useState({});
-  const[customPopup,setCustomPopup]=useState(false)
-  
-  useEffect(() =>{
+  const [searchField, setSearchField] = useState('');
+  const [isOpenPopup, setOpenPopup] = useState(false);
+  const [clickedDog, setClickedDog] = useState({});
+  const [customPopup, setCustomPopup] = useState(false)
+
+  useEffect(() => {
     fetch(`https://dog.ceo/api/breeds/list/all`)
-            .then(response => response.json())
-            .then(data => {
-              setNames(Object.keys(data.message))
-              // console.log(DogNames)
-            })
-          }, [])
-  const handleChange=(search)=>{
-   setSearchField(search);
+      .then(response => response.json())
+      .then(data => {
+        setNames(Object.keys(data.message))
+        // console.log(DogNames)
+      })
+  }, [])
+  const handleChange = (search) => {
+    setSearchField(search);
     // console.log(search); 
-    }
-  const filteredDogName=names.filter((name) => {
-      return name.toLowerCase().includes(searchField.toLowerCase())
-    })
+  }
+  const filteredDogName = names.filter((name) => {
+    return name.toLowerCase().includes(searchField.toLowerCase())
+  })
 
   const userClickedHandler = (selectDogBreed) => {
     console.log("user is clicked")
     setOpenPopup(true);
     setClickedDog(selectDogBreed);
-    }
+  }
   const closePopup = () => {
     setOpenPopup(false);
-    }
+  }
 
   const customClickedHandler = () => {
-      console.log("user is clicked")
-      setCustomPopup(true);
-    }
+    console.log("user is clicked")
+    setCustomPopup(true);
+  }
   const CustomeClosePopup = () => {
     setCustomPopup(false);
-    }
-    return (
-      <>
-        {isOpenPopup ? <Popup clickedDogName={clickedDog} closePopup={closePopup}/>:null}
-        {customPopup ? <CustomPopup CustomeClosePopup={CustomeClosePopup}/> : null}
-        <CustomSearch customClickedHandler={customClickedHandler}/> 
-        <SearchBox handleChange={handleChange}/>
-        <DogCardList names={names,filteredDogName} userClickedHandler={userClickedHandler}/>
-      </>
-    );
+  }
+  return (
+    <>
+      {isOpenPopup ? <Popup clickedDogName={clickedDog} closePopup={closePopup} /> : null}
+      {customPopup ? <CustomPopup CustomeClosePopup={CustomeClosePopup} /> : null}
+      <CustomSearch customClickedHandler={customClickedHandler} />
+      <SearchBox handleChange={handleChange} />
+      <DogCardList names={names, filteredDogName} userClickedHandler={userClickedHandler} />
+    </>
+  );
 }
 export default App;
